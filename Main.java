b54316bedad84f6993bcd355538b1c7a5766714a
Grayson Scott <scott.grace9@gmail.com>
2025-01-30 05:42:23 +0000

    added new print line

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from Grayson");
+
+        System.out.println("This is a change.");
     }
 }
